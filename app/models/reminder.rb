ac5bbc8d507e87538fbabf37872fc21e56148050
koheitakahashi <kouhei.takahashi@everyleaf.com>
2022-03-28 21:00:54 +0900

    画面からIntervalSecondsを1日単位で登録できようにした

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -3,4 +3,12 @@ class Reminder < ApplicationRecord
     reminders = Reminder.where(remind_at: (5.minutes.ago)..(5.minutes.since))
     reminders.each(&:remind)
   end
+
+  def interval_days=(days)
+    self.interval_seconds = days.to_i.days
+  end
+
+  def interval_days
+    self.interval_seconds / (60 * 60 * 24)
+  end
 end
