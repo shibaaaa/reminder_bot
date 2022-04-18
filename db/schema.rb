f51ffc0284c78eaddc4bcd06f662d7b57fe0e26d
Kosuke Shibata <k.shibata9447@gmail.com>
2022-04-18 20:47:09 +0900

    remindersテーブルにカラム追加
    リマインダーを飛ばすチャンネルを入力できるようにするため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_21_112004) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_18_113046) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_21_112004) do
     t.string "type", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "webhook_url", null: false
+    t.string "channel_name", null: false
     t.index ["remind_at"], name: "index_reminders_on_remind_at"
   end
 
